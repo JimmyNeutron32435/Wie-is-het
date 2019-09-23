cd98483dbdfb3fe5e42b8b06a102f53992a702e0
Jamy <jamy.kreeft.jr@gmail.com>
2019-09-23 12:10:28 +0200

    afgemaakt

---
+++ kaas2.js
@@ -0,0 +1,85 @@
+var geel = prompt("Is de kaas geel?");
+
+
+		if(geel == "ja"){
+
+		var gaten =	prompt("Zitten er gaten in?");
+
+	}
+
+		if (geel == "nee"){
+
+		var blauw =	prompt("Heeft de kaas blauwe schimmel?");
+
+	}
+
+		if(gaten == "ja"){
+
+		var duur = prompt("Is de kaas belachelijk duur?");
+
+	}
+
+		if (gaten == "nee"){
+
+		var steen = prompt("Is de kaas zo hard als steen?");
+
+	}
+
+		if(duur == "ja"){
+
+		alert("Uw kaas is: Emmenthaler");
+
+	}
+
+		if(duur == "nee"){
+
+		alert("Uw kaas is: Leerdammer");
+
+	}
+
+		if(steen == "ja"){
+
+		alert("Uw kaas is: Pamigiano Reggiano");
+
+	}
+
+		if (steen == "nee"){
+
+		alert("Uw kaas is: Goudse kaas");
+
+	}
+
+		if (blauw == "ja"){
+
+		var korst = prompt("Heeft de kaas een korst?");
+
+	}
+
+		if (blauw == "nee"){
+
+		var korst2 = prompt("Heeft de kaas een korst?");
+
+	}
+
+		if (korst == "ja"){
+
+		alert("Uw kaas is: Bleu de Rochbaron");
+
+	}
+
+		if (korst == "nee"){
+
+		alert("Uw kaas is: Foume d'Ambert");
+
+	}
+
+		if (korst2 == "ja"){
+
+		alert("Uw kaas is: Camembert");
+
+	}
+
+		if (korst2 == "nee"){
+
+		alert("Uw kaas is: Mozzarella");
+	}
